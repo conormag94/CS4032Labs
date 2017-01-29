e39d800cd01b4dfe9389ed3d3fa35b5099293619
Conor Maguire <conormag94@gmail.com>
2017-01-29 14:07:00 +0000

    Add run script, also Lock service can now insert a test Document to a mongoDB database through a rest call

--- Project/lock-service/src/LockService.hs
+++ Project/lock-service/src/LockService.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
@@ -16,10 +17,13 @@ import Prelude.Compat
 import Control.Monad.IO.Class
 import Control.Monad.Except
 import Control.Monad.Reader
-import Data.Aeson.Types
-import Data.Aeson.TH
+import Control.Monad.Trans.Resource
+import Data.Aeson
+import Data.Bson
 import Data.Proxy as DP
-import Data.List
+import Data.List hiding (insert)
+import Database.MongoDB
+import Database.MongoDB
 import GHC.Generics
 import Network.HTTP.Client
 import Network.Wai
@@ -29,6 +33,7 @@ import Servant.API
 import Servant.Client
 import System.IO
 import System.Directory
+import qualified Data.Text as DT
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Lazy.IO as TextIO
 
@@ -42,6 +47,9 @@ data FileLock = FileLock {
 
 type LockAPI = "locktest" :> Get '[JSON] ResponseMessage
 
+insertTest :: Document
+insertTest = ["name" =: ("insertTest.txt" :: String), "data" =: ("It worked" :: String)]
+
 startApp :: IO ()
 startApp = do 
   putStrLn "Lock server running on localhost:8082"
@@ -53,7 +61,8 @@ lockServer = locktest
   where
     locktest :: Handler ResponseMessage
     locktest = liftIO $ do
-      return $ ResponseMessage {response = "Lock server up and running"}
+      insertId <- withMongoDbConnection $ insert "locks" insertTest
+      return $ ResponseMessage {response = "ID: " ++ (show insertId)}
 
 
 lockApi :: DP.Proxy LockAPI
@@ -61,3 +70,21 @@ lockApi = DP.Proxy
 
 app :: Application
 app = serve lockApi lockServer
+
+withMongoDbConnection :: Action IO a -> IO a
+withMongoDbConnection act  = do
+  let ip = "127.0.0.1"
+  let database = "locks"
+  pipe <- connect (Database.MongoDB.host ip)
+  ret <- runResourceT $ liftIO $ access pipe master (DT.pack database) act
+  close pipe
+  return ret
+
+drainCursor :: Cursor -> Action IO [Document]
+drainCursor cur = drainCursor' cur []
+  where
+    drainCursor' cur res  = do
+      batch <- nextBatch cur
+      if batch == []
+        then return res
+        else drainCursor' cur (res ++ batch)
